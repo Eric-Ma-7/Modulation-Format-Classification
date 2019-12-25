ddad8e9acfaf3435a4745f63b26917e0135fa2db
Eric-Ma-7 <39188334+Eric-Ma-7@users.noreply.github.com>
2019-12-25 14:18:31 +0800

    Update signal_generation.m

--- src/signal_generation.m
+++ src/signal_generation.m
@@ -9,21 +9,27 @@ function RxSignal = signal_generation(bitPerSymbol,snr,format,varargin)
 %     RxSignal - output signals
 if nargin == 3
     dataNum = 10000;
-else
+elseif nargin == 4
     dataNum = varargin{1};
+elseif nargin == 5
+    dataNum = varargin{1};
+    phzNoise = varargin{2};
 end
 
 M = 2^bitPerSymbol;
-dataSymbol = randi([0,M-1],1,dataNum);
+dataSymbol = randi([0,M-1],dataNum,1);
 if strcmp(format,'psk')||strcmp(format,'PSK')
 	TxSignal = pskmod(dataSymbol,M,0);
 elseif strcmp(format,'qam')||strcmp(format,'QAM')
     if bitPerSymbol == 3
-        constel = [1+1i, -1+1i, -1-1i, 1-1i, 1+sqrt(3), (1+sqrt(3))*1i, -1-sqrt(3), -(1+sqrt(3))*1i];
+        constel = [1+1i; -1+1i; -1-1i; 1-1i; 1+sqrt(3); (1+sqrt(3))*1i; -1-sqrt(3); -(1+sqrt(3))*1i];
         TxSignal = constel(dataSymbol+1);
     else
         TxSignal = qammod(dataSymbol,M,'gray');
     end
+end
+if nargin == 5
+    TxSignal = phzNoise(TxSignal);
 end
 RxSignal = awgn(TxSignal,snr,'measured');
 end
